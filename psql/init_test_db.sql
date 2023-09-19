87c8e9509964a88e0ad385a688fb0a88816d8c58
cortze <mikelcortes013@gmail.com>
2023-09-19 20:07:24 +0200

    support for github actions

---
+++ psql/init_test_db.sql
@@ -0,0 +1,26 @@
+-- Create default database for testing:
+CREATE DATABASE test_database;
+
+-- Allow user to read/write from the db:
+GRANT ALL PRIVILEGES ON DATABASE test_database to test_user;
+
+-- Create custom tables for the testing
+CREATE TABLE IF NOT EXISTS test_table(
+	id INT,
+	text_col TEXT,
+	attribute_col TEXT,
+	bool_col BOOL,
+
+	PRIMARY KEY(id));
+
+-- Insert dummy data into test_table
+INSERT INTO test_table(
+	id,
+	text_col,
+	attribute_col,
+	bool_col)
+VALUES
+(1, 'first', 'attr_1', false),
+(2, 'second', null,  true),
+(3, 'third', 'attr_1', false),
+(4, 'fourth', 'attr_3', true);
