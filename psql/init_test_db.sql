e705fcc8cd1b4f874e5dacbc47c298cbfd313d47
cortze <mikelcortes013@gmail.com>
2023-09-19 20:31:28 +0200

    a

--- psql/init_test_db.sql
+++ psql/init_test_db.sql
@@ -1,20 +1,3 @@
--- Create custom tables for the testing
-CREATE TABLE IF NOT EXISTS test_table(
-	id INT,
-	text_col TEXT,
-	attribute_col TEXT,
-	bool_col BOOL,
+-- Create default database for testing:
+CREATE DATABASE test_database;
 
-	PRIMARY KEY(id));
-
--- Insert dummy data into test_table
-INSERT INTO test_table(
-	id,
-	text_col,
-	attribute_col,
-	bool_col)
-VALUES
-(1, 'first', 'attr_1', false),
-(2, 'second', null,  true),
-(3, 'third', 'attr_1', false),
-(4, 'fourth', 'attr_3', true);
