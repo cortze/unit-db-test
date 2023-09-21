4cc31b37807b2257ac0139591799926e4efacfb7
cortze <cortze@protonmail.com>
2023-09-21 16:59:46 +0200

    update the env file reading utils

--- unit_db_test/utils.py
+++ unit_db_test/utils.py
@@ -1,18 +1,39 @@
 # utils.py
 import os
+import os.path
 from dotenv import load_dotenv
 
+class NoEnvVariableError(Exception):
+    def __init__(self, var_name: str):
+        self.var = var_name
 
-def load_credentials_from(file):
+    def cause(self) -> str:
+        return self.var
+
+
+def load_credentials_from(file: str):
+    check_if_exists(file)
     _ = load_dotenv(file)
-    return get_db_from_env()
+    return get_db_credentials_from_env()
 
 
-def get_db_from_env():
-    user = os.getenv('DB_USER')
-    password = os.getenv('DB_PASSWORD')
-    host = os.getenv('DB_HOST')
-    port = os.getenv('DB_PORT')
-    database = os.getenv('DB_DATABASE')
-    url = os.getenv('DB_URL')
-    return user, password, host, port, database, url
+def check_if_exists(file: str):
+    if not os.path.exists(file):
+        raise FileNotFoundError(file)
+
+
+def read_env_var(var_name: str):
+    var = os.getenv(var_name)
+    if var is None:
+        raise NoEnvVariableError(var_name)
+    else:
+        return var
+
+
+def get_db_credentials_from_env():
+    user = read_env_var('DB_USER')
+    password = read_env_var('DB_PASSWORD')
+    host = read_env_var('DB_HOST')
+    port = read_env_var('DB_PORT')
+    database = read_env_var('DB_DATABASE')
+    return user, password, host, port, database
