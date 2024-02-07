c46bd01daa81fadef7ac95620a0eb07da705e2b1
cortze <cortze@protonmail.com>
2024-02-07 13:30:14 +0100

    attempt2

--- psql/docker_init_postgresql_test_db.sql
+++ psql/docker_init_postgresql_test_db.sql
@@ -1,6 +1,7 @@
 -- Create custom tables for the testing
 CREATE TABLE IF NOT EXISTS test_table(
 	id INT,
+    integer  INT,
 	text_col TEXT,
 	attribute_col TEXT,
 	bool_col BOOL,
@@ -10,11 +11,12 @@ CREATE TABLE IF NOT EXISTS test_table(
 -- Insert dummy data into test_table
 INSERT INTO test_table(
 	id,
+    integer,
 	text_col,
 	attribute_col,
 	bool_col)
 VALUES
-(1, 'first', 'attr_1', false),
-(2, 'second', null,  true),
-(3, 'third', 'attr_1', false),
-(4, 'fourth', 'attr_3', true);
+(1, 1, 'first', 'attr_1', false),
+(2, 2, 'second', null,  true),
+(3, NULL, 'third', 'attr_1', false),
+(4, 4, 'fourth', 'attr_3', true);
